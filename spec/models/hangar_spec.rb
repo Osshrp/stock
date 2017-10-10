4394f177c822088a1cc55a402e6162e55b71329a
Osshrp <osshrp@gmail.com>
2017-10-10 12:48:28 +0300

    add hangar#sum_wieght spec

--- spec/models/hangar_spec.rb
+++ spec/models/hangar_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Hangar, type: :model do
   it { should have_many(:products) }
   it { should validate_presence_of :number }
+
+  let(:hangar) { create(:hangar) }
+  let(:product) { create(:product, hangar: hangar) }
+  let!(:item1) { create(:item, product: product) }
+  let!(:item2) { create(:item, product: product) }
+
+  describe '#sum_wieght' do
+    it 'calculate sum of items wieght' do
+      expect(hangar.sum_wieght).to eq(item1.total_wieght + item2.total_wieght)
+    end
+  end
 end
